ffa29ba3f0bb624d14e30b5a235e8517ae006520
arno2546 <arno.ahmed2546@gmail.com>
2020-09-21 20:55:43 +0600

    basic guide index implemented

--- Scripts/GuideScripts/index.js
+++ Scripts/GuideScripts/index.js
@@ -7,4 +7,12 @@ $(document).ready(function () {
     console.log(sessionStorage.getItem('guideCon'));
     //console.log(sessionStorage.getItem('guidePass'));
     console.log(sessionStorage.getItem('guideMail'));
+    var uname = sessionStorage.getItem('guideUname');
+    var password = sessionStorage.getItem('guidePass');
+    var mail = sessionStorage.getItem('guideMail');
+    var id = sessionStorage.getItem('guideId');
+    var contact = sessionStorage.getItem('guideCon');
+    $("title").html("Chaparone: "+uname);
+    $("#logOut").html("Logout("+uname+")");
+    $("#WelcomeLable").html("Welcome, "+uname+" (Guide)");
 });
