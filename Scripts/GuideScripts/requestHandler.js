40092214ec77ac8c6a2bb5a0cbeef66fca92050f
arno2546 <arno.ahmed2546@gmail.com>
2020-09-23 10:06:33 +0600

    Basic Guide Dash done

--- Scripts/GuideScripts/requestHandler.js
+++ Scripts/GuideScripts/requestHandler.js
@@ -1,8 +1,66 @@
 $(document).ready(function () {
+   
+    console.log(getParameter('reqId'));
+    console.log(getParameter('action'));
+    var reqId = getParameter('reqId');
+    var reqState = getParameter('action');
+    
+    getRequest();
+    
+    
     function getParameter( paramName ){
         let parameters = new URLSearchParams(window.location.search);
         return parameters.get( paramName );
     }
-    console.log(getParameter('reqId'));
-    console.log(getParameter('action'));
+
+    function getRequest(){
+        $.ajax({
+            url: "https://localhost:44337/api/requests/"+reqId,           
+            method:"get",
+            complete: function(xmlHttp,status){
+                if(xmlHttp.status==200){
+                    var data = xmlHttp.responseJSON;
+                    tuoristId = data.TouristId;
+                    guideId = data.GuideId;
+                    starDate = data.StartDate;
+                    endDate = data.EndDate;
+                    reqLocation = data.Location;                   
+                    putRequest();
+                }
+                else{
+                    console.log(xmlHttp.status+":"+xmlHttp.statusText); 
+                }
+            }
+        });
+    }
+    function putRequest(){
+        $.ajax({
+            url: "https://localhost:44337/api/requests/"+reqId,           
+            method:"put",
+            headers:{
+                contentType:"application.json"
+            },
+            data:{
+                Location: reqLocation,
+                TouristId: tuoristId,
+                GuideId: guideId,
+                StartDate: starDate,
+                EndDate: endDate,
+                RequestState: reqState
+            },
+            complete:function(xmlHttp,status){
+                if(xmlHttp.status==200){
+                    alert("Successfully Completed Request");
+                    window.location.href = "http://localhost/ChaperoneFront/Views/GuideViews/";
+                }
+                else{
+                    $("#postCreateMsg").html("Error: "+xmlHttp.status+":"+xmlHttp.statusText);
+                    console.log(xmlHttp.status+":"+xmlHttp.statusText); 
+                }
+            }
+       })
+    }
+
+
+
 });
